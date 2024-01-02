efd789f2ba7f345da4860338d2bcc80128a3bc19
ackieeee <akiyama85k@gmail.com>
2024-01-03 01:36:55 +0900

    配列返却検証

--- recipient1_server/main.go
+++ recipient1_server/main.go
@@ -11,46 +11,46 @@ import (
 )
 
 var (
-  recipients = []*pb.Recipient{
-    {
-      RepientId: 1,
-      Email: "test1@test.email.com",
-    },
-    {
-      RepientId: 2,
-      Email: "test2@test.email.com",
-    },
-    {
-      RepientId: 3,
-      Email: "test3@test.email.com",
-    },
-  }
+	recipients = []*pb.Recipient{
+		{
+			RepientId: 1,
+			Email:     "test1@test.email.com",
+		},
+		{
+			RepientId: 2,
+			Email:     "test2@test.email.com",
+		},
+		{
+			RepientId: 3,
+			Email:     "test3@test.email.com",
+		},
+	}
 )
 
 type Recipient1Server struct {
-  pb.UnimplementedRecipienterServer
+	pb.UnimplementedRecipienterServer
 }
 
 func (s *Recipient1Server) GetRecipients(ctx context.Context, in *pb.GetRecipientsRequest) (*pb.GetRecipientsResponse, error) {
-  targets := []*pb.Recipient{}
-  for _, recipient := range recipients {
-    recipientIds := in.GetRecipientIds()
-    if slices.Index[int64](recipientIds, recipient.RepientId) > -1 {
-      targets = append(targets, recipient)
-    }
-  }
-  return *pb.GetRecipientsResponse{
-    Recipient: targets,
-  }, nil
+	targets := []*pb.Recipient{}
+	for _, recipient := range recipients {
+		recipientIds := in.GetRecipientIds()
+		if slices.Contains[[]int64, int64](recipientIds, recipient.RepientId) {
+			targets = append(targets, recipient)
+		}
+	}
+	return &pb.GetRecipientsResponse{
+		Recipient: targets,
+	}, nil
 }
 
 func main() {
-  lis, err := net.Listen("tcp", ":50090")
+	lis, err := net.Listen("tcp", ":50090")
 	if err != nil {
 		log.Fatal("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-  server := &Recipient1Server{}
+	server := &Recipient1Server{}
 	pb.RegisterRecipienterServer(s, server)
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
---
+++ recipient_client/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	pb "github.com/ackieeee/grpc-sample/sample"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50090", "the address to connect to")
+)
+
+func main() {
+	flag.Parse()
+
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewRecipienterClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	// r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
+	request := pb.GetRecipientsRequest{
+		RecipientIds: []int64{1, 2, 4, 3},
+	}
+	res, err := c.GetRecipients(ctx, &request)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	recipients := res.GetRecipient()
+	for _, recipient := range recipients {
+		log.Printf("res: %#v", recipient.RepientId)
+	}
+}
